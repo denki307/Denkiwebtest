16b9a6d7fc3d3334e8ef0495b96d690b9d38c2ca
cookies2002 <cookies2002boy@gmail.com>
2026-02-13 20:54:59 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,3 @@
-// Tab Switch
-const tabs = document.querySelectorAll(".tab");
-const contents = document.querySelectorAll(".tab-content");
-
-tabs.forEach(tab => {
-    tab.addEventListener("click", () => {
-        tabs.forEach(t => t.classList.remove("active"));
-        contents.forEach(c => c.classList.remove("active"));
-
-        tab.classList.add("active");
-        document.getElementById(tab.dataset.tab).classList.add("active");
-    });
-});
-
 // Live Status Check
 fetch("/status")
 .then(res => res.json())
@@ -20,10 +6,13 @@ fetch("/status")
     const text = document.getElementById("statusText");
 
     if (data.status === "online") {
-        dot.style.background = "#00ff88";
+        dot.style.background = "#00ffaa";
         text.innerText = "Online";
     } else {
         dot.style.background = "red";
         text.innerText = "Offline";
     }
+})
+.catch(() => {
+    document.getElementById("statusText").innerText = "Unknown";
 });
